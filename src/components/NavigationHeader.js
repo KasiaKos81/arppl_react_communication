43481b664c92967b8808b5694fdc0d6d09f47ee3
Kasia <kasiakos81@gmail.com>
2022-06-21 20:11:18 +0200

    210622

--- src/components/NavigationHeader.js
+++ src/components/NavigationHeader.js
@@ -9,7 +9,27 @@ const NavigationHeader = () => {
                         <p>ARPPL</p>
                     </div>
                 </div>
+                <div className={classes.Prawa}>
+                    <div className={classes.TextPrawa}>
+                        <p>
+                            <button>Formularz</button>
+                        </p>
+                    </div>
+                    <div className={classes.TextPrawa}>
+                        <p>
+                            <button>Lista</button>
+                        </p>
+                    </div>
+                    <div className={classes.TextPrawa}>
+                        <p>
+                            <button>Home</button>
+                        </p>
+                    </div>
 
+
+                </div>
+            </div>
+            <div>
             </div>
         </header>
     );
